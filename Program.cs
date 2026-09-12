11599f50cf6eaddfde50b40284e2ddee8eb7fad6
marwan-2004 <marwanelzobeer659@gmail.com>
2026-09-12 22:36:02 +0300

    fix: handle division by zero and input validation

--- Program.cs
+++ Program.cs
@@ -39,7 +39,12 @@ namespace Calculator
                         Console.WriteLine($"Result: {num1} * {num2} = {num1 * num2}");
                         break;
                     case "4":
-                        
+                        if(num2 ==0)
+                        {
+                            Console.WriteLine("Error: Division by zero is not allowed.");
+                        }
+                        else
+                        {
                             Console.WriteLine($"Result: {num1} / {num2} = {num1 / num2}");
                          break;
                     default:
